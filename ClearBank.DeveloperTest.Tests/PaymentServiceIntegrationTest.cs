e68d69e469a367e4b843dd4da02e79fa67c5f6e8
LoZeno <lozeno1982@gmail.com>
2021-01-27 10:58:45 +0000

    first end-to-end test, to allow refactoring

---
+++ ClearBank.DeveloperTest.Tests/PaymentServiceIntegrationTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Configuration;
+using ClearBank.DeveloperTest.Services;
+using ClearBank.DeveloperTest.Types;
+using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    public class PaymentServiceIntegrationTest
+    {
+        [Fact]
+        public void WhenRequestIsValid_AndAccountHasNoAllowedPayments_ReturnsFailedPayment()
+        {
+            ConfigurationManager.AppSettings["DataStoreType"] = "NonBackup";
+
+            var paymentService = new PaymentService();
+
+            var makePaymentRequest = new MakePaymentRequest
+            {
+                PaymentScheme = PaymentScheme.FasterPayments
+            };
+            
+            var paymentResult = paymentService.MakePayment(makePaymentRequest);
+            
+            Assert.False(paymentResult.Success);
+        }
+    }
+}
