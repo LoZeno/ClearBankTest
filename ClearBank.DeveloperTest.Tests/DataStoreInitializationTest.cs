572cd03e207d04322e143341b75bb06c6460f432
LoZeno <lozeno1982@gmail.com>
2021-01-27 11:59:56 +0000

    moved datastore initialization logic to a injection container

---
+++ ClearBank.DeveloperTest.Tests/DataStoreInitializationTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Configuration;
+using ClearBank.DeveloperTest.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ClearBank.DeveloperTest.Tests
+{
+    public class DataStoreInitializationTest
+    {
+        [Fact]
+        public void WhenDataStoreInConfigIsBackup_InitializesBackup()
+        {
+            ConfigurationManager.AppSettings["DataStoreType"] = "Backup";
+
+            var ioc = new InjectionContainer();
+
+            var accountDataStore = ioc.GetProvider().GetRequiredService<IAccountDataStore>();
+
+            Assert.IsType<BackupAccountDataStore>(accountDataStore);
+        }
+        
+        [Fact]
+        public void WhenDataStoreInConfigIsMain_InitializesDataStore()
+        {
+            ConfigurationManager.AppSettings["DataStoreType"] = "Main";
+
+            var ioc = new InjectionContainer();
+
+            var accountDataStore = ioc.GetProvider().GetRequiredService<IAccountDataStore>();
+
+            Assert.IsType<AccountDataStore>(accountDataStore);
+        }
+        
+        [Fact]
+        public void WhenDataStoreInConfigIsNotValid_ThrowsConfigurationErrorsException()
+        {
+            ConfigurationManager.AppSettings["DataStoreType"] = "NonValid";
+
+            Assert.Throws<ConfigurationErrorsException>(() => new InjectionContainer());
+        }
+    }
+}
---
+++ ClearBank.DeveloperTest/InjectionContainer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Configuration;
+using ClearBank.DeveloperTest.Data;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ClearBank.DeveloperTest
+{
+    public class InjectionContainer
+    {
+        private readonly IServiceCollection _serviceCollection;
+
+        public InjectionContainer()
+        {
+            _serviceCollection = new ServiceCollection();
+            var dataStoreSetting = ConfigurationManager.AppSettings["DataStoreType"];
+
+            _serviceCollection = dataStoreSetting switch
+            {
+                "Backup" => _serviceCollection.AddSingleton<IAccountDataStore, BackupAccountDataStore>(),
+                "Main" => _serviceCollection.AddSingleton<IAccountDataStore, AccountDataStore>(),
+                _ => throw new ConfigurationErrorsException()
+            };
+        }
+        public IServiceProvider GetProvider()
+        {
+            return _serviceCollection.BuildServiceProvider();
+        }
+    }
+}
